6a55986fec4c52641cab5e8edb4c88bd13cf2a5f
DimaSurdiy <dsurdiy@ukr.net>
2022-07-21 06:16:15 +0300

    add Statistics component

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -29,9 +29,9 @@ export function Profile({ username, tag, location, avatar, stats }) {
 }
 
 Profile.propTypes = {
-  username: PropTypes.string,
-  tag: PropTypes.string,
-  location: PropTypes.string,
-  avatar: PropTypes.string,
-  stats: PropTypes.object,
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.object.isRequired,
 };
