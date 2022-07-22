0f0fce0e234e097d9eab6350749474599b0528db
DimaSurdiy <dsurdiy@ukr.net>
2022-07-22 05:20:13 +0300

    Profile path bug fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Profile } from 'components/Profile/Profile';
+// import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
 import user from 'user.json';
@@ -8,13 +8,13 @@ import friends from 'friends.json';
 export function App() {
   return (
     <div>
-      <Profile
+      {/* <Profile
         username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
-      />
+      /> */}
 
       <Statistics title="Upload stats" stats={data} />
 
