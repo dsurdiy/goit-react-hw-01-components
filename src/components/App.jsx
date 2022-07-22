9dc26e18966041756c1f0e3b46648994b0d59ef8
DimaSurdiy <dsurdiy@ukr.net>
2022-07-22 05:24:13 +0300

    user bug fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 // import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
-import user from 'user.json';
+// import user from 'user.json';
 import data from 'data.json';
 import friends from 'friends.json';
 
